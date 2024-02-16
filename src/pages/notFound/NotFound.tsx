b7e0df47e4def4313adf93697a18b561aa48c23d
Wilson <77452536+wilson-kbs@users.noreply.github.com>
2024-02-16 04:27:13 +0100

    feat: implement not found page

--- src/pages/notFound/NotFound.tsx
+++ src/pages/notFound/NotFound.tsx
@@ -10,13 +10,22 @@ function NotFound() {
   }, []);
 
   return (
-    <div className={styles.NotFound}>
-      <h1>{content.body.title}</h1>
-      <p>{content.body.message}</p>
-      <Link to={content.body.links.backToHome.href}>
-        {content.body.links.backToHome.title}
-      </Link>
-    </div>
+    <section className={styles.NotFound}>
+      <header className={styles.NotFound__Header}>
+        <h1 className={styles.NotFound__title}>{content.body.title}</h1>
+      </header>
+      <main className={styles.NotFound__Main}>
+        <p className={styles.NotFound__message}>{content.body.message}</p>
+      </main>
+      <footer className={styles.NotFound__Footer}>
+        <Link
+          className={styles.NotFound__link}
+          to={content.body.links.backToHome.href}
+        >
+          {content.body.links.backToHome.title}
+        </Link>
+      </footer>
+    </section>
   );
 }
 
